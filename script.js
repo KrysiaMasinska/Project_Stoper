cdf8c2e2456b70dfb273e18063084bf2f51b9e1e
Krysia Masińska <krysiamasinska@gmail.com>
2021-07-30 00:17:42 +0200

    Fixed btns stoper, added hover root color

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ const btnPause = document.querySelector('.pause');
 const btnStop = document.querySelector('.stop');
 const btnReset = document.querySelector('.reset');
 const btnArchive = document.querySelector('.archive');
+const btnClose = document.querySelector('.close');
+const btnOne = document.querySelector('.one');
+const btnTwo = document.querySelector('.two');
+const btnThree = document.querySelector('.three');
 
 const timer = document.querySelector('.timer');
 const timerStop = document.querySelector('.timer_stop')
 const timerList = document.querySelector('.timer_list');
+const modalShadow = document.querySelector('.modal_shadow');
+const modalPaint = document.querySelector('.modal_paint')
 
 let counter = 0;
 let minutes = 0;
@@ -42,22 +48,21 @@ const handleStop = () => {
 
     timerStop.textContent = `Ostatni czas: ${timer.textContent}`;
 
-    if(timer.textContent !== '0:00'){
+    if (timer.textContent !== '0:00') {
         timerStop.style.visibility = 'visible';
         lastTime.push(timer.textContent);
-        console.log(lastTime);
     }
 
     clearStock();
 }
 
-const handleReset = () =>{
+const handleReset = () => {
     timerStop.style.visibility = 'hidden';
     lastTime = [];
     clearStock();
 }
 
-const clearStock = () =>{
+const clearStock = () => {
     clearInterval(counter);
     timer.textContent = '0:00';
     timerList.textContent = '';
@@ -65,9 +70,9 @@ const clearStock = () =>{
     minutes = 0;
 }
 
-const showArchive = () =>{
+const showArchive = () => {
     let number = 1;
-    lastTime.forEach(time =>{
+    lastTime.forEach(time => {
         const newLi = document.createElement('li');
         newLi.innerHTML = `Pomiar ${number}: <span>${time}</span>`;
         timerList.appendChild(newLi);
@@ -75,8 +80,44 @@ const showArchive = () =>{
     })
 }
 
+const showModal = () => {
+
+    showItem(modalShadow);
+}
+
+const showBrush = () => {
+    showItem(modalPaint);
+}
+
+const showItem = item => {
+    if (!(item.style.display === 'block')) {
+        item.style.display = 'block';
+    } else {
+        item.style.display = 'none';
+    }
+
+    item.classList.toggle('animation_modal')
+}
+
+const one = () => {
+    document.documentElement.setAttribute("style", '--mainColor: orangered; --hoverColor: red');
+}
+const two = () => {
+    document.documentElement.setAttribute("style", "--mainColor: blue; --hoverColor: rgb(46,84,165)");
+}
+const three = () => {
+    document.documentElement.setAttribute("style", "--mainColor: green; --hoverColor: rgb(70,142,70)");
+}
+
 btnPlay.addEventListener('click', start);
 btnPause.addEventListener('click', pauseInterval);
-btnStop.addEventListener('click',handleStop);
+btnStop.addEventListener('click', handleStop);
 btnReset.addEventListener('click', handleReset);
 btnArchive.addEventListener('click', showArchive);
+question.addEventListener('click', showModal);
+btnClose.addEventListener('click', showModal);
+window.addEventListener('click', e => e.target === modalShadow ? showModal() : false);
+brush.addEventListener('click', showBrush);
+btnOne.addEventListener('click', one);
+btnTwo.addEventListener('click', two);
+btnThree.addEventListener('click', three);
